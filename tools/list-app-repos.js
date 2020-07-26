bf2ad6f64594bc25b12a863caec4894fcc58ac98
Lnk2past <Lnk2past@gmail.com>
2020-07-26 16:56:35 -0400

    add log statements

--- tools/list-app-repos.js
+++ tools/list-app-repos.js
@@ -4,7 +4,9 @@ var github = require('../github')
 var githubConfig = config.githubConfig
 
 async function main() {
+    console.log(githubConfig)
     var token = await auth.getToken(githubConfig.appID, githubConfig.installationID, githubConfig.clientID, githubConfig.clientSecret, githubConfig.privateKeyPath)
+    console.log(token)
     var orgRepositories = await github.getOrgRepos(token, 'ruc-sci-comp')
     for (repo of orgRepositories) {
         console.log(repo)
