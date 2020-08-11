a53fdabbf2a121ed61bed96aeae2c90d299d5a4f
Lnk2past <Lnk2past@gmail.com>
2020-08-10 23:29:10 -0400

    update register await object

--- index.js
+++ index.js
@@ -184,24 +184,24 @@ client.on('message', async msg => {
     if (msg.content.startsWith('!register')) {
         var dmChannel = await msg.author.createDM();
         var filter = m => m.content.length != 0;
-        var timeout = 30000;
+        var messageAwaitObject = { max: 1, time: timeout, errors: ['time'] };
         var count = await db.countUser(pool, msg.author.id);
         if (count > 0) {
             dmChannel.send('`This Discord account is already registered! Contact your instructor.`')
             return;
         }
         dmChannel.send('`Enter your full name (First Last) / [or cancel to quit]`').then(() => {
-            dmChannel.awaitMessages(filter, { max: 1, time: timeout, errors: ['time'] }).then(name => {
+            dmChannel.awaitMessages(filter, messageAwaitConfig).then(name => {
                 if (name.first().content.toLowerCase() == 'cancel') {
                     return;
                 }
                 dmChannel.send('`Enter your GitHub Username / [or cancel to quite]`').then(() => {
-                    dmChannel.awaitMessages(filter, { max: 1, time: timeout, errors: ['time'] }).then(githubUserName => {
+                    dmChannel.awaitMessages(filter, messageAwaitConfig).then(githubUserName => {
                         if (githubUserName.first().content.toLowerCase() == 'cancel') {
                             return;
                         }
                         dmChannel.send(`\`Are you sure you want to proceed? This cannot be undone! [yes/no]\`\n\`Name: ${name.first().content}\`\n\`GitHub: ${githubUserName.first().content}\``).then(() => {
-                            dmChannel.awaitMessages(filter, { max: 1, time: timeout, errors: ['time'] }).then(confirmation => {
+                            dmChannel.awaitMessages(filter, messageAwaitConfig).then(confirmation => {
                                 if (confirmation.first().content.toLowerCase() == 'yes') {
                                     db.registerUser(pool, name.first().content, msg.author.id, githubUserName.first().content).then( rowCount => {
                                         if (rowCount == 1) {
