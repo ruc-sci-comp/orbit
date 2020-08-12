0abbdd29715ed32e76641b4d77c90e3b1e1e3c87
Lnk2past <Lnk2past@gmail.com>
2020-08-11 22:29:13 -0400

    ...

--- github/queries/index.js
+++ github/queries/index.js
@@ -4,6 +4,6 @@ module.exports = {
     getProjectIdQuery: fs.readFileSync('github/queries/get_project_id.graphql', 'utf8'),
     getCardsQuery: fs.readFileSync('github/queries/get_cards.graphql', 'utf8'),
     getReposQuery: fs.readFileSync('github/queries/get_repos.graphql', 'utf8'),
-    getReposQuery: fs.readFileSync('github/queries/get_repos_for_team.graphql', 'utf8'),
+    getReposForTeamQuery: fs.readFileSync('github/queries/get_repos_for_team.graphql', 'utf8'),
     getGradeIssuesForUserQuery: fs.readFileSync('github/queries/get_grade_issues_for_user.graphql', 'utf8')
 }
