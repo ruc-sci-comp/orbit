2c808c96c808c37bd239ddb27e2a0c2a28f0b858
Lnk2past <Lnk2past@gmail.com>
2020-08-12 00:18:52 -0400

    use other export syntax

--- utilities/index.js
+++ utilities/index.js
@@ -1,53 +1,57 @@
-module.exports = 
-{
-    getMemberById: function(guild, id) {
-        return guild.members.cache.find(member => member.id === id);
-    },
-
-    getRoleByName: function(guild, role) {
-        return guild.roles.cache.find(role => role.name === role);
-    },
-
-    send_dm: async function(msg, content) {
-        return msg.author.send(content);
-    },
-    
-    send_text: async function(msg, content) {
-        return msg.channel.send(content);
-    },
-    
-    send_message: async function(msg, content) {
-        if (msg.channel.type == 'dm') {
-            return send_dm(msg, content);
-        }
-        else {
-            return send_text(msg, content);
-        }
-    },
-
-    createChannel: async function(guild, name, type, parent=undefined) {
-        for (var channel of guild.channels.cache.values()) {
-            if (channel.name == name && channel.type == type) {
-                return channel.id;
-            }
-        }
-        var new_channel = await guild.channels.create(
-            name,
-            {
-                type: type,
-                parent: parent,
-                permissionOverwrites: [
-                    {
-                        id: guild.roles.everyone,
-                        deny: ['VIEW_CHANNEL'],
-                    },
-                    {
-                        id: getRoleByName("Student").id,
-                        allow: ['VIEW_CHANNEL'],
-                    },
-                ],
-            }
-        )
-        return new_channel.id
-    },
+function getMemberById(guild, id) {
+    return guild.members.cache.find(member => member.id === id);
 }
+
+function getRoleByName(guild, role) {
+    return guild.roles.cache.find(role => role.name === role);
+}
+
+async function send_dm(msg, content) {
+    return msg.author.send(content);
+}
+
+async function send_text(msg, content) {
+    return msg.channel.send(content);
+}
+
+async function send_message(msg, content) {
+    if (msg.channel.type == 'dm') {
+        return send_dm(msg, content);
+    }
+    else {
+        return send_text(msg, content);
+    }
+}
+
+async function createChannel(guild, name, type, parent=undefined) {
+    for (var channel of guild.channels.cache.values()) {
+        if (channel.name == name && channel.type == type) {
+            return channel.id;
+        }
+    }
+    var new_channel = await guild.channels.create(
+        name,
+        {
+            type: type,
+            parent: parent,
+            permissionOverwrites: [
+                {
+                    id: guild.roles.everyone,
+                    deny: ['VIEW_CHANNEL'],
+                },
+                {
+                    id: getRoleByName("Student").id,
+                    allow: ['VIEW_CHANNEL'],
+                },
+            ],
+        }
+    )
+    return new_channel.id
+}
+
+exports.getMemberById = getMemberById;
+exports.getRoleByName = getRoleByName;
+exports.send_dm = send_dm;
+exports.send_text = send_text;
+exports.send_message = send_message;
+exports.createChannel = createChannel;
